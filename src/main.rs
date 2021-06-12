7e7fe05931eb20bd8b170f7a21311f448b685f28
Herculean <55994229+Herkulisch@users.noreply.github.com>
2021-06-12 16:07:08 +0200

    Program now reads the path as start parameter and starts the rom execution on it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 #![allow(unused_imports, dead_code)]
+use chip::Chip8;
+use std::env;
 mod chip;
 fn main() {
+    for (i, arg) in env::args().enumerate() {
+        if i == 1 {
+            let mut chip = Chip8::new();
+            chip.start_rom(arg);
+        }
+    }
 }

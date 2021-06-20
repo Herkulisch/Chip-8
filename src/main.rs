067d6f0163e7b7da90667b950f7065ac2d4a30a4
Herculean <55994229+Herkulisch@users.noreply.github.com>
2021-06-20 21:11:38 +0200

    added the missing front_end module to main method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![allow(unused_imports, dead_code)]
 use chip::Chip8;
 use std::env;
+
 mod chip;
+mod front_end;
+
 fn main() {
     for (i, arg) in env::args().enumerate() {
         if i == 1 {
